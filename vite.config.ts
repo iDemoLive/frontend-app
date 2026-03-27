40bf74b098b0d6c7aaca15ff47311b74209cde43
idemo if2u <if2u.app@gmail.com>
2026-03-27 14:41:53 +0700

    edit vite path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/frontend-app/',
+  // base: '/frontend-app/',  // use for github only not docker
   plugins: [react()],
 })
